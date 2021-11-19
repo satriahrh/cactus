dbdefa89cf1c4a146b52f27860af118a1b3f0e01
Satria H R Harsono <satriahrh@gmail.com>
2021-11-20 01:26:39 +0700

    location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,63 @@
 import {
+  IonButton,
   IonContent,
-  IonHeader,
+  IonIcon,
+  IonItem,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  IonText,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
+import { notificationsOutline } from "ionicons/icons";
 import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Beranda</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Beranda</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Beranda page" />
+        <IonItem
+          class="ion-no-margin"
+          lines="none"
+          style={{
+            marginTop: "32px",
+          }}
+        >
+          <IonText class="ion-padding-start">
+            <h1
+              style={{
+                fontWeight: "semibold",
+                margin: 0,
+              }}
+            >
+              Baleendah
+            </h1>
+            <br />
+            <p
+              style={{
+                margin: 0,
+              }}
+            >
+              Bandung, Jawa Barat
+            </p>
+          </IonText>
+          <IonButton
+            shape="round"
+            fill="clear"
+            size="large"
+            slot="end"
+            style={{
+              textAlign: "top",
+            }}
+          >
+            <IonIcon
+              color="dark"
+              icon={notificationsOutline}
+              style={{
+                innerHeight: "31px",
+              }}
+            />
+          </IonButton>
+        </IonItem>
+
+        <IonItem lines="none"></IonItem>
       </IonContent>
     </IonPage>
   );
