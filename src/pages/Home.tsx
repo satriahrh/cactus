0176d5ed2964617a51b0875b70860110bd3556ed
Satria H R Harsono <satriahrh@gmail.com>
2021-11-20 01:34:32 +0700

    separate location data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,54 +12,62 @@ import "./Home.css";
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <IonItem
-          class="ion-no-margin"
-          lines="none"
+      <Location district="Baleendah" cityProvince="Bandung, Jawa Barat" />
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+type LocationProps = {
+  district: String;
+  cityProvince: String;
+};
+
+const Location = (props: LocationProps) => {
+  return (
+    <IonItem
+      class="ion-no-margin"
+      lines="none"
+      style={{
+        marginTop: "32px",
+      }}
+    >
+      <IonText class="ion-padding-start">
+        <h1
           style={{
-            marginTop: "32px",
+            fontWeight: "semibold",
+            margin: 0,
           }}
         >
-          <IonText class="ion-padding-start">
-            <h1
-              style={{
-                fontWeight: "semibold",
-                margin: 0,
-              }}
-            >
-              Baleendah
-            </h1>
-            <br />
-            <p
-              style={{
-                margin: 0,
-              }}
-            >
-              Bandung, Jawa Barat
-            </p>
-          </IonText>
-          <IonButton
-            shape="round"
-            fill="clear"
-            size="large"
-            slot="end"
-            style={{
-              textAlign: "top",
-            }}
-          >
-            <IonIcon
-              color="dark"
-              icon={notificationsOutline}
-              style={{
-                innerHeight: "31px",
-              }}
-            />
-          </IonButton>
-        </IonItem>
-
-        <IonItem lines="none"></IonItem>
-      </IonContent>
-    </IonPage>
+          {props.district}
+        </h1>
+        <br />
+        <p
+          style={{
+            margin: 0,
+          }}
+        >
+          {props.cityProvince}
+        </p>
+      </IonText>
+      <IonButton
+        shape="round"
+        fill="clear"
+        size="large"
+        slot="end"
+        style={{
+          textAlign: "top",
+        }}
+      >
+        <IonIcon
+          color="dark"
+          icon={notificationsOutline}
+          style={{
+            innerHeight: "31px",
+          }}
+        />
+      </IonButton>
+    </IonItem>
   );
 };
 
