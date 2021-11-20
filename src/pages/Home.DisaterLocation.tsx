62cf70e4640e3629b4d93c286b7695545e29e8e4
Satria H R Harsono <satriahrh@gmail.com>
2021-11-20 15:05:02 +0700

    disaster locations dummy component

--- src/pages/Home.DisaterLocation.tsx
+++ src/pages/Home.DisaterLocation.tsx
@@ -1,6 +1,20 @@
-import { IonText } from "@ionic/react";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonHeader,
+  IonImg,
+  IonList,
+  IonRow,
+  IonText,
+  IonThumbnail,
+} from "@ionic/react";
 import { DataGetDisaterLocationsType } from "./Home.Types";
 import * as React from "react";
+import "./Home.css";
 
 type DisaterLocationProps = {
   dataGetDisaterLocations: DataGetDisaterLocationsType;
@@ -11,7 +25,73 @@ const DisasterLocation = React.memo(
     console.log(dataGetDisaterLocations);
     return (
       <>
-        <IonText>Kawasan Banjir Saat Ini</IonText>
+        <IonText>
+          <h3
+            style={{
+              fontWeight: "semi-bold",
+              fontSize: "14px",
+              margin: "24px 0px 16px 8px",
+            }}
+          >
+            Kawasan Banjir Saat Ini
+          </h3>
+        </IonText>
+        {dataGetDisaterLocations?.map((value, index) => {
+          return (
+            <IonCard
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <IonCardContent>
+                <IonRow className="ion-no-padding">
+                  <IonCol
+                    style={{
+                      maxWidth: "78px",
+                      marginRight: "10px",
+                    }}
+                  >
+                    <IonThumbnail className="disaster-location-thumbnail">
+                      <img
+                        alt="Ilustrasi Gambar Banjir"
+                        src="https://disk.mediaindonesia.com/thumbs/1800x1200/news/2020/10/cdb6fae9bdff54319546bb32fdd8edf3.jpg"
+                      />
+                    </IonThumbnail>
+                  </IonCol>
+                  <IonCol>
+                    <IonText color="dark">
+                      <h2
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: "16px",
+                        }}
+                      >
+                        {value.district}, {value.city}
+                      </h2>
+                      <p
+                        style={{
+                          fontSize: "12px",
+                        }}
+                      >
+                        kedalaman banjir <strong>{value.floodSeverity}</strong>
+                      </p>
+                    </IonText>
+                    <IonText color="grey">
+                      <p
+                        style={{
+                          fontSize: "10px",
+                        }}
+                      >
+                        {value.timestamp}
+                      </p>
+                    </IonText>
+                  </IonCol>
+                </IonRow>
+              </IonCardContent>
+            </IonCard>
+          );
+        })}
       </>
     );
   }
