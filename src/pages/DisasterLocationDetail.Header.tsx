e8fe122be7f06cfa9d53300c36cd3acf0cd4ed77
Satria H R Harsono <satriahrh@gmail.com>
2021-11-20 21:29:16 +0700

    separating header

---
+++ src/pages/DisasterLocationDetail.Header.tsx
@@ -0,0 +1,58 @@
+import {
+  IonBackButton,
+  IonButtons,
+  IonHeader,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { DataGetDisaterLocationType } from "./Home.Types";
+import "./DisasterLocationDetail.css";
+
+type HeaderProps = {
+  dataGetDisasterLocation: DataGetDisaterLocationType;
+};
+
+const Header = ({ dataGetDisasterLocation }: HeaderProps) => {
+  return (
+    <IonHeader className="header">
+      <IonToolbar className="toolbar">
+        <IonButtons slot="start">
+          <IonBackButton color="light" />
+        </IonButtons>
+      </IonToolbar>
+      <IonTitle
+        color="light"
+        style={{
+          fontWeight: "bold",
+          fontSize: "16px",
+        }}
+      >
+        {dataGetDisasterLocation.district}, {dataGetDisasterLocation.city}
+      </IonTitle>
+      <div className="ion-margin">
+        <IonText color="light">
+          <p
+            style={{
+              margin: 0,
+              fontSize: "12px",
+            }}
+          >
+            Kedalaman banjir{" "}
+            <strong>{dataGetDisasterLocation.floodSeverity}</strong>
+          </p>
+          <p
+            style={{
+              margin: 0,
+              fontSize: "10px",
+            }}
+          >
+            {dataGetDisasterLocation.timestamp}
+          </p>
+        </IonText>
+      </div>
+    </IonHeader>
+  );
+};
+
+export default Header;
--- src/pages/DisasterLocationDetail.tsx
+++ src/pages/DisasterLocationDetail.tsx
@@ -1,18 +1,10 @@
-import {
-  IonBackButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonText,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { DataGetDisaterLocationType } from "./Home.Types";
 import "./DisasterLocationDetail.css";
 import ThreadsListing from "./DisasterLocationDetail.ThreadsListing";
+import Header from "./DisasterLocationDetail.Header";
 
 // type DisasterLocationDetailProps = {
 //   dataGetDisasterLocation: DataGetDisaterLocationType;
@@ -43,52 +35,6 @@ const DisasterLocationDetail = () => {
         )}
       </IonContent>
     </IonPage>
-  );
-};
-
-type HeaderProps = {
-  dataGetDisasterLocation: DataGetDisaterLocationType;
-};
-
-const Header = ({ dataGetDisasterLocation }: HeaderProps) => {
-  return (
-    <IonHeader className="header">
-      <IonToolbar className="toolbar">
-        <IonButtons slot="start">
-          <IonBackButton color="light" />
-        </IonButtons>
-      </IonToolbar>
-      <IonTitle
-        color="light"
-        style={{
-          fontWeight: "bold",
-          fontSize: "16px",
-        }}
-      >
-        {dataGetDisasterLocation.district}, {dataGetDisasterLocation.city}
-      </IonTitle>
-      <div className="ion-margin">
-        <IonText color="light">
-          <p
-            style={{
-              margin: 0,
-              fontSize: "12px",
-            }}
-          >
-            Kedalaman banjir{" "}
-            <strong>{dataGetDisasterLocation.floodSeverity}</strong>
-          </p>
-          <p
-            style={{
-              margin: 0,
-              fontSize: "10px",
-            }}
-          >
-            {dataGetDisasterLocation.timestamp}
-          </p>
-        </IonText>
-      </div>
-    </IonHeader>
   );
 };
 
